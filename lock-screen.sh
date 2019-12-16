088ec8a512d59e42ce4e279b4c68a5308e4d8b4a
fridokus <raxomukus@gmail.com>
2019-12-16 16:25:03 +0100

    work on lock screen

--- lock-screen.sh
+++ lock-screen.sh
@@ -2,4 +2,5 @@
 #shutter -f -e -o /tmp/lockscreen
 scrot /tmp/lockscreen.png
 convert /tmp/lockscreen.png -blur 0x6 /tmp/lockscreen.png
+#convert /tmp/lockscreen.png -resize 10% -resize 1000% /tmp/lockscreen.png
 i3lock -i /tmp/lockscreen.png
